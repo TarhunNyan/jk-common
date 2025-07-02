d5f7ab558661bb30cf1cd1ad30abb678028e4f08
g.stepanov <g.stepanov.sis@gmail.com>
2025-07-03 06:50:26 +0700

    docs(api): добавить комментарии с пояснениями

--- src/api/java/sg01/common/api/di/ISimpleContainer.java
+++ src/api/java/sg01/common/api/di/ISimpleContainer.java
@@ -3,6 +3,9 @@ package sg01.common.api.di;
 import sg01.common.api.di.isp.IHaveRegister;
 import sg01.common.api.di.isp.IHaveResolve;
 
+/**
+ * EFP-1: [container]
+ */
 public interface ISimpleContainer extends IContainer, IHaveRegister, IHaveResolve {
 
 }
